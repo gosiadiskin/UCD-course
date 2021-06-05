2233557e8971b6586b7919fc953ff7eb67a4fe12
gosiadiskin <gosiadiskin@gmail.com>
2021-06-05 13:34:04 +0100

    Initialiazed new project. Added Main, Test and csv

---
+++ Test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data=pd.read_csv("BankChurners.csv")
+
+print(data)
+print(data["CLIENTNUM"])
+print(data.shape)
+print(data.info())
+print(data.describe())
+print(data.describe().transpose())
+print(data.head(5))
+
+python.exe -m pip install --upgrade pip
+
+
